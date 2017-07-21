861ec46986cb5d3c432a156d7b01aa1a4f4cfc32
Jacob H. Rothschild <Jacob.R.Developer@gmail.com>
2017-07-22 00:18:23 +0200

    Performed some simple, surface-level restructuring of model.server.Reader.java

--- app/src/main/java/rothschild/henning/jacob/noriginmedia/model/server/Reader.java
+++ app/src/main/java/rothschild/henning/jacob/noriginmedia/model/server/Reader.java
@@ -5,10 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Locale;
-import java.util.logging.Logger;
 
 /**
  * Created by Jacob Rothschild on 08.10.2016.
@@ -16,71 +12,53 @@ import java.util.logging.Logger;
  * Could have been made static, but Dependency Injection is preferred for cleaner project structure.
  */
 class Reader {
+	
+	private static final String TAG = Reader.class.getSimpleName() + ".";
 
-	private static final Logger log = Logger.getLogger(Reader.class.getName());
-
-	/**
-	 * @return The full HTML, in one long String
-	 */
+	/** @return The full HTML, in one, large String */
 	String readHTML(String urlString) {
-		return arrayListToString(urlStringToHTML(urlString));
-	}
-
-	/**
-	 * @return The result of calling '.toString()' on each elements within 'strings' and adding the results together
-	 */
-	private <T> String arrayListToString(ArrayList<T> list) {
-		StringBuilder stringBuilder = new StringBuilder();
-		for (T line : list) stringBuilder.append(line);
-		return stringBuilder.toString();
-	}
-
-	private ArrayList<String> urlStringToHTML(String urlString) {
 		try {
-			return unsafe_urlStringToHTML(urlString);
+			return urlStringToHTML(urlString);
 		} catch (Exception e) {
-			log.severe("ERROR encountered in 'urlStringToHTML()', or more specifically in 'unsafe_urlStringToHTML()'; urlString = " + urlString);
-			log.severe(Arrays.toString(e.getStackTrace()));
+			e.printStackTrace();
 		}
 		return null;
 	}
-
-	private ArrayList<String> unsafe_urlStringToHTML(String urlString) throws Exception {
-		return extractHTMLFromURL(stringToURL(urlString));
-	}
-
-	/**
-	 * @return If possible an ArrayList<String> of all lines of the html behind 'url'
-	 */
-	private ArrayList<String> extractHTMLFromURL(URL url) throws IOException {
-		// Settings "UTF-8" is necessary to read "æøå". This is NOT reproducible in normal java, but it is in GAE
-		try {
-			BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
-			ArrayList<String> output = bufferedReaderToArrayList(reader);
-			reader.close();
-			return output;
-		} catch (IOException e) {
-			e.printStackTrace();
-			log.info(String.format(new Locale("no"), "Reader.extractHTMLFromURL() threw exception: url.toString() = %s, url.getFile() = %s, url.getHost() = %s, url.getPath() = %s",
-					url.toString(), url.getFile(), url.getHost(), url.getPath()));
-			throw e;
-		}
+	
+	/** @throws Exception All methods called by this method re-throw their exceptions upward. Thus, this method will throw a MalformedUTLException or IOException, if anything, anywhere goes wrong between url-string (input) and html-string (output) */
+	private String urlStringToHTML(String urlString) throws Exception {
+		return urlToHTML(stringToURL(urlString));
 	}
 	
-	/**
-	 * @return An ArrayList<String> made by all the contents of 'reader'
-	 */
-	private ArrayList<String> bufferedReaderToArrayList(BufferedReader reader) throws IOException {
-		ArrayList<String> output = new ArrayList<>();
-		String line;
-		while ((line = reader.readLine()) != null) output.add(line);
-		return output;
-	}
-
-	/**
-	 * @return A URL-object created from 'urlString'
-	 */
+	/** @return A URL-object created from 'urlString' */
 	private URL stringToURL(String urlString) throws MalformedURLException {
 		return new URL(urlString);
+	}
+
+	/** @return If possible, the whole HTML behind 'url', in one, large String */
+	private String urlToHTML(URL url) throws IOException {
+		BufferedReader reader = urlToBufferedReader(url);
+		String output = bufferedReaderToHTML(reader);
+		reader.close();
+		return output;
+	}
+	
+	/** @return A BufferedReader for fetching the HTML of 'url' */
+	private BufferedReader urlToBufferedReader(URL url) throws IOException {
+		// Settings "UTF-8" is necessary to read "æøå". This is NOT reproducible in normal java, but it is in GAE
+		return new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
+	}
+	
+	/** @return A long String made by all the contents of 'reader' */
+	private String bufferedReaderToHTML(BufferedReader reader) throws IOException {
+		return bufferedReaderToStringBuilder(reader).toString();
+	}
+	
+	/** @return A StringBuilder made up of all the contents of 'reader' */
+	private StringBuilder bufferedReaderToStringBuilder(BufferedReader reader) throws IOException {
+		StringBuilder stringBuilder = new StringBuilder();
+		String line;
+		while ((line = reader.readLine()) != null) stringBuilder.append(line);
+		return stringBuilder;
 	}
 }
